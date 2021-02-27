5d67e070ba870037221f52dd5fcdf814a409a9f5
jyung <huntthetux@gmail.com>
2021-02-27 14:44:09 +0800

    add console logging in winston

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -5,6 +5,7 @@ const logger = winston.createLogger({
   format: winston.format.json(),
   defaultMeta: { service: 'ride-service' },
   transports: [
+    new winston.transports.Console(),
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'info.log' }),
   ],
